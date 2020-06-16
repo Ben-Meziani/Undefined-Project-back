8312856123430e9f9a412a28e289fd46e2f44f57
ben <toufik-m@live.fr>
2020-06-16 15:37:35 +0200

    Start register

--- back/src/Controller/RegistrationController.php
+++ back/src/Controller/RegistrationController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-
+use Symfony\Component\HttpFoundation\Response;
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/register", name="app_registration", methods={"GET"})
+     * @Route("/register", name="app_registration", methods={"POST"})
      */
     public function register(Request $request, SerializerInterface $serializer)
     {
         $jsonRecu = $request->getContent();
+        
+        $post = $serializer->deserialize($jsonRecu, User::class, 'json');
+        dd($post);
     }
 }
