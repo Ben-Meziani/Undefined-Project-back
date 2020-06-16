c3658a618c67507a6c93c616a260565f669dcbf2
ben <toufik-m@live.fr>
2020-06-16 15:47:05 +0200

    register + comments password

--- back/src/Controller/RegistrationController.php
+++ back/src/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +15,18 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="app_registration", methods={"POST"})
      */
-    public function register(Request $request, SerializerInterface $serializer)
+    public function register(Request $request, SerializerInterface $serializer, EntityManager $em)
     {
         $jsonRecu = $request->getContent();
         
         $post = $serializer->deserialize($jsonRecu, User::class, 'json');
-        dd($post);
+        
+        //$mdp = $post->getPassword();
+        //on encrypte le mdp
+        //$post->setPassword(le mdp encrypter)
+
+        //persist et flush  
+       // $em->persist($post);
+       // $em->flush();
     }
 }
