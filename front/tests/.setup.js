c5d3f9a32c35ede7ee6a0a29d40ef8ffeb50d234
REMY-Valentin <valentin.remy88@gmail.com>
2020-06-21 15:02:21 +0200

    remove front

--- front/tests/.setup.js
+++
@@ -1,6 +0,0 @@
-require('@babel/register')();
-require('ignore-styles');
-const enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-enzyme.configure({ adapter: new Adapter() });
