1180e36f881424cfbe297b10d935a80dd3d9c4fe
REMY-Valentin <valentin.remy88@gmail.com>
2020-06-23 16:26:35 +0200

    persist launch in bdd

--- back/src/Controller/DiceController.php
+++ back/src/Controller/DiceController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Dice;
+use App\Entity\Room;
 use App\Entity\User;
+use DateTime;
 use PhpParser\ErrorHandler\Throwing;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,7 +24,7 @@ class DiceController extends AbstractController
         $json = $request->getContent();
         $params = json_decode($json);
         $resultat = $this->apiDice($params->dice,$params->launch);
-
+        $this->save($user, $resultat, $params->dice, $params->room);
 
         return $this->json($resultat, 200);
     }
@@ -31,5 +34,24 @@ class DiceController extends AbstractController
         $resultat =  file_get_contents("https://www.dejete.com/api?nbde=$dice&tpde=$launch");
         
         return $resultat;
+    }
+
+    private function save(User $user, $resultat, $diceType, $roomId) 
+    {
+        $resultatDecoded = json_decode($resultat);
+        $room = $this->getDoctrine()->getRepository(Room::class)->find($roomId);
+        //dd($resultatDecoded);
+        foreach($resultatDecoded as $launch) {
+            $dice = new Dice;
+            $dice->setLaunchBy($user->getPseudo());
+            $dice->setResult($launch->value);
+            $dice->setType($diceType);
+            $dice->setCreatedAt(new DateTime());
+            $dice->addRoomId($room);
+            $dice->addUserId($user);
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($dice);
+            $manager->flush();
+        }
     }
 }
