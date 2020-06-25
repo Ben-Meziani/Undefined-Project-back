e87fa49cb58dd90e2e7c1abff41bee35086831e7
REMY-Valentin <valentin.remy88@gmail.com>
2020-06-25 10:06:59 +0200

    liip imagine bundle

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
