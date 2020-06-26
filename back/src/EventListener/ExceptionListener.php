c54c12c0ff0f49f82b5c0bfcdaa320d7808df0b2
REMY-Valentin <valentin.remy88@gmail.com>
2020-06-26 12:00:06 +0200

    test cookie

--- back/src/EventListener/ExceptionListener.php
+++ back/src/EventListener/ExceptionListener.php
@@ -3,13 +3,18 @@ namespace App\EventListener;
 
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class ExceptionListener
 {
     public function onKernelException(AuthenticationSuccessEvent $event)
     {
         $user = $event->getUser();
-        //dd($user->getId());
+        $cookie = Cookie::create('test')
+            ->withValue('if this work OMG')
+            ->withDomain('.test.com')
+            ->withSecure(false);
+        $event->getResponse()->headers->setcookie($cookie);
         $event->setData(['id'=>$user->getId(),'pseudo'=>$user->getPseudo(),'email'=>$user->getEmail(), 'icon'=>$user->getIcon()]);
     }
 }
