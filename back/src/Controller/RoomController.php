8e7011a30d05c9784ae56da456659f3e6a72825e
REMY-Valentin <valentin.remy88@gmail.com>
2020-07-04 16:03:10 +0200

    test

--- back/src/Controller/RoomController.php
+++ back/src/Controller/RoomController.php
@@ -31,7 +31,7 @@ class RoomController extends AbstractController
     {
         $room = $this->getDoctrine()->getRepository(Room::class)->findOneBy(['uuid' => $id]);
         if ($request->isMethod('POST')) {
-            $file = $request->files->get('image');
+            $file = $request->files;
             dd($room, $file);
             
             if ($file) {
