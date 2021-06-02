6becf0a7c63a34ada40679649f83b1a3cb082a7b
Ben-web-developement <toufik-m@live.fr>
2021-06-02 16:13:26 +0200

    oUIE?C%L§QE?KCNV KELQN

--- back/src/Controller/ContactController.php
+++ back/src/Controller/ContactController.php
@@ -14,6 +14,7 @@ class ContactController extends AbstractController
      */
     public function index(Request $request, \Swift_Mailer $mailer)
     {
+        yooooooooooooooooooooooooooooo
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
